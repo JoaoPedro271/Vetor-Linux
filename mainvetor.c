c39ff0aeca25af0d5605b1ba4d1b4fe42ff546ff
Joao Pedro Soares Almeida <joao.almeida325@etec.sp.gov.br>
2023-11-01 16:33:37 -0300

    Atividade vetor Criada

---
+++ mainvetor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+
+	int n1,lista;
+
+	printf("Digite um valor ");
+	scanf("%d",&n1);
+
+	for(lista=0; lista <13; lista = lista + n1){
+
+
+		printf("Voce adicionou: %d ",lista);
+
+	}
+
+}
+
